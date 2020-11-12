73188bf632a3cdd60208f856deb265743cc8882c
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-12 14:20:38 -0600

    added date to header

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//current day is displayed at the top of the calendar
+    var currentTime = moment().format("dddd MMMM Do, YYYY");
+    $("#currentDay")
+    .text("Today is " + currentTime);
+//time blocks for standard business hours
+//time block is color-coded to indicate whether it is in the past, present, or future
+//click into a time block can enter an event
+
+//click the save button for that time block text for that event is saved in local storage
+
+
+//refresh the page saved events persist
