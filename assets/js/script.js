0f0e4cfa27110036b32a7f75b945df0490769563
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-14 10:21:09 -0600

    saving and retrieving from local storage is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
-//current day is displayed at the top of the calendar
-var currentTime = moment().format("dddd MMMM Do, YYYY");
-$("#currentDay")
-.text("Today is " + currentTime);
-//time blocks for standard business hours
-//time block is color-coded to indicate whether it is in the past, present, or future
-//click into a time block can enter an event
-
-//click the save button for that time block text for that event is saved in local storage
+//this makes the background gray when a user clicks into the time slot; reverts back to white when they click out
+    $('textarea').focus(function(){
+        $(this).css('background', "#F2F3F4");
+    });
+    $('textarea').blur(function(){
+        $(this).css('background', "#fff");
+    });
 
 
-//refresh the page saved events persist
+// save button clicks and saving to local storage
+$(".saveBtn").on("click", function() {
+    //get values of text entry and time it is in
+    var taskText = $(this).siblings(".description").val();
+    var taskTime = $(this).parent().attr("id");
+    //save items to local storage
+    localStorage.setItem(taskTime, taskText);
+});
+
+
+
+//loading local storage items
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+
