5c2eabe14238289364c9558c8e9a7781710812d6
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-15 19:44:12 -0600

    added README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+### Functions of this scheduler are: 
+ * Current date is displayed at the top of the page
+ * Times are set up in hour blocks from 9am-5pm
+ * Add events or tasks in the time block
+ * When user presses save, this will save to Local Storage 
+ * When user reloads page, the saved data will be there
+
+### Functions to be added:
+* When time block is in the past, it will show as gray
+* When time block is in the future, it will show as green
+* When time block is current, it will show as red 
+
+
+You can preview the quiz live by clicking the link below:
+https://steftinsley.github.io/work-day-scheduler/
+
+
+Mockup of the site is below:
+
+![alt text](https://github.com/steftinsley/code-quiz/blob/master/assets/screenshot%20of%20code%20quiz.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
 
-//Adding date to header
-var currentTime = moment().format('MMMM Do YYYY, h:mm a');
-$("#currentDay")
-.text(currentTime);
-
-
-//this makes the background gray when a user clicks into the time slot; reverts back to white when they click out
-    $('textarea').focus(function(){
-        $(this).css('background', "#F2F3F4");
+    //Adding date to header
+    var currentTime = moment().format('MMMM Do YYYY, h:mm a');
+    $("#currentDay")
+    .text(currentTime);
+    
+    
+    //this makes the background gray when a user clicks into the time slot; reverts back to white when they click out
+        $('textarea').focus(function(){
+            $(this).css('background', "#F2F3F4");
+        });
+        $('textarea').blur(function(){
+            $(this).css('background', "#fff");
+        });
+    
+    
+    // save button clicks and saving to local storage
+    $(".saveBtn").on("click", function() {
+        //get values of text entry and time it is in
+        var taskText = $(this).siblings(".description").val();
+        var taskTime = $(this).parent().attr("id");
+        //save items to local storage
+        localStorage.setItem(taskTime, taskText);
     });
-    $('textarea').blur(function(){
-        $(this).css('background', "#fff");
+    
+    
+    
+    //loading local storage items
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+    $("#5 .description").val(localStorage.getItem("5"));
+    
     });
-
-
-// save button clicks and saving to local storage
-$(".saveBtn").on("click", function() {
-    //get values of text entry and time it is in
-    var taskText = $(this).siblings(".description").val();
-    var taskTime = $(this).parent().attr("id");
-    //save items to local storage
-    localStorage.setItem(taskTime, taskText);
-});
-
-
-
-//loading local storage items
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#1 .description").val(localStorage.getItem("1"));
-$("#2 .description").val(localStorage.getItem("2"));
-$("#3 .description").val(localStorage.getItem("3"));
-$("#4 .description").val(localStorage.getItem("4"));
-$("#5 .description").val(localStorage.getItem("5"));
-
-});
