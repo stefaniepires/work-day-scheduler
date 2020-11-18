933a5f7e22e3c834a6b24828c4d662d3e1bbf281
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-18 01:32:27 -0600

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,86 @@
-$(document).ready(function() {
+//Adding date to header
+var currentTime = moment().format("MMMM Do YYYY, h:mm a");
+$("#currentDay").text(currentTime);
 
-    //Adding date to header
-    var currentTime = moment().format('MMMM Do YYYY, h:mm a');
-    $("#currentDay")
-    .text(currentTime);
+//select all time blocks and text areas
+var timeBlocks = Array.from(document.querySelectorAll(".time-block"));
+
+var addEvent = Array.from(document.querySelectorAll(".addEvent"));
+
+//this is for grabbing the current hour
+var currentHour = moment().hour();
+
+//array for the events that user inputs
+var events = [];
+
+var hourColorUpdate = function () {
+
+  for (var i = 0; i < timeBlocks.length; i++) {
+
+    var timeBlockId = timeBlocks[i].firstElementChild.id;
+
+    var blockHour = timeBlockId.replace("hour-", "");
+
+    if (blockHour < 9) {
+        blockHour = parseInt(blockHour) + 12;
+    } else {
+        blockHour = parseInt(blockHour);
+    }
+
+    if (blockHour === parseInt(currentHour)) {
+      var presentTime = document.getElementById(
+        JSON.stringify(blockHour)
+      );
+    $(presentTime).addClass("present");
+    } else if (blockHour> currentHour) {
+
+      var presentTime= document.getElementById(
+        JSON.stringify(blockHour)
+      );
+
+      $(presentTime).addClass("future");
+    }
+  }
+};
+
+
+hourColorUpdate();
+
+
+
+var loadTasks = function() {
+
+    events = localStorage.getItem('events')
     
-    
-    //this makes the background gray when a user clicks into the time slot; reverts back to white when they click out
-        $('textarea').focus(function(){
-            $(this).css('background', "#F2F3F4");
-        });
-        $('textarea').blur(function(){
-            $(this).css('background', "#fff");
-        });
-    
-    
-    // save button clicks and saving to local storage
+    if(events === null) {
+        events = []
+    }
+    else{
+         events = JSON.parse(events);
+    }
+
+    for (var i = 0; i < events.length; i++){
+
+        
+   
+     $(addEvent[i]).text(events[i]);
+
+    }
+
+};
+
+loadTasks();
+
+
+// saved to local storage when save button is clicked
+
     $(".saveBtn").on("click", function() {
-        //get values of text entry and time it is in
-        var taskText = $(this).siblings(".description").val();
-        var taskTime = $(this).parent().attr("id");
-        //save items to local storage
-        localStorage.setItem(taskTime, taskText);
+
+        var userInput = $(this).siblings(".addEvent").val()
+
+        events.push(userInput);
+
+        localStorage.setItem('events', JSON.stringify(events))    
+
     });
-    
-    
-    
-    //loading local storage items
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#1 .description").val(localStorage.getItem("1"));
-    $("#2 .description").val(localStorage.getItem("2"));
-    $("#3 .description").val(localStorage.getItem("3"));
-    $("#4 .description").val(localStorage.getItem("4"));
-    $("#5 .description").val(localStorage.getItem("5"));
-    
-    });
+
